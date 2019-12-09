ee51351dd9699f31596babf0536e5a26ac365c2e
svwoodhouse <svwoodhouse@gmail.com>
2019-12-08 20:59:22 -0500

    Added HelloWorld project and first assignment

---
+++ Chapter2Assignments/SumOfTwoNumbers/SumOfTwoNumbers/Source.cpp
@@ -0,0 +1,17 @@
+/* This program puts the numbers
+   62 and 99 in two integer variables and  stores 
+   their sum into the total variable*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1 = 62;
+	int num2 = 99;
+	int total = num1 + num2;
+
+	cout << num1 << " + " << num2 << " = " << total;
+	return 0;
+}
